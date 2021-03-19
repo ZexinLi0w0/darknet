24ee65ccebaee07510e030bcbb8acbd3ad2c6dd3
ZexinLi0w0 <lizexin0903@gmail.com>
2021-03-19 02:49:18 -0500

    data process script

--- data_process_yolo.py
+++ data_process_yolo.py
@@ -15,11 +15,13 @@ def data_process(args):
         pattern = re.compile(r'\d+.\d+')
         value_raw = float(pattern.findall(fps)[0])
         FPS.append(value_raw)
+        if value_raw < 20.0:
+            count += 1 # miss
   # print(FPS)
 #   print("Average FPS: {}".format(sum(FPS)/len(FPS)))
 #   print("Min/Max FPS: {}, {}".format(min(FPS), max(FPS)))
   
-  return sum(FPS)/len(FPS), min(FPS), max(FPS)
+  return sum(FPS)/len(FPS), min(FPS), max(FPS), count / len(FPS)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
@@ -29,23 +31,23 @@ if __name__ == '__main__':
     
     print("avg min max")
     # 1 inst
-    avg1, minv1, maxv1 = data_process("test-1.txt")
-    print("1 inst: {}, {}, {}".format(avg1, minv1, maxv1))
+    avg1, minv1, maxv1, pmiss1 = data_process("test-1.txt")
+    print("1 inst: {}, {}, {}, {}".format(avg1, minv1, maxv1, pmiss1))
     
     # 2 inst
-    avg1, minv1, maxv1 = data_process("test-2.1.txt")
-    avg2, minv2, maxv2 = data_process("test-2.2.txt")
-    print("2 inst: {}, {}, {}".format((avg1+avg2)/2, min(minv1,minv2), max(maxv1,maxv2)))
+    avg1, minv1, maxv1, pmiss1 = data_process("test-2.1.txt")
+    avg2, minv2, maxv2, pmiss2 = data_process("test-2.2.txt")
+    print("2 inst: {}, {}, {}, {}".format((avg1+avg2)/2, min(minv1,minv2), max(maxv1,maxv2), (pmiss1+pmiss2)/2))
 
     # 3 inst
-    avg1, minv1, maxv1 = data_process("test-3.1.txt")
-    avg2, minv2, maxv2 = data_process("test-3.2.txt")
-    avg3, minv3, maxv3 = data_process("test-3.3.txt")
-    print("3 inst: {}, {}, {}".format((avg1+avg2+avg3)/3, min(minv1,minv2,minv3), max(maxv1,maxv2,maxv3)))
+    avg1, minv1, maxv1, pmiss1 = data_process("test-3.1.txt")
+    avg2, minv2, maxv2, pmiss2 = data_process("test-3.2.txt")
+    avg3, minv3, maxv3, pmiss3 = data_process("test-3.3.txt")
+    print("3 inst: {}, {}, {}, {}".format((avg1+avg2+avg3)/3, min(minv1,minv2,minv3), max(maxv1,maxv2,maxv3), (pmiss1+pmiss2+pmiss3)/3))
     
     # 4 inst
-    avg1, minv1, maxv1 = data_process("test-4.1.txt")
-    avg2, minv2, maxv2 = data_process("test-4.2.txt")
-    avg3, minv3, maxv3 = data_process("test-4.3.txt")
-    avg4, minv4, maxv4 = data_process("test-4.4.txt")
-    print("4 inst: {}, {}, {}".format((avg1+avg2+avg3+avg4)/4, min(minv1,minv2,minv3,minv4), max(maxv1,maxv2,maxv3,maxv4)))
+    avg1, minv1, maxv1, pmiss1 = data_process("test-4.1.txt")
+    avg2, minv2, maxv2, pmiss2 = data_process("test-4.2.txt")
+    avg3, minv3, maxv3, pmiss3 = data_process("test-4.3.txt")
+    avg4, minv4, maxv4, pmiss4 = data_process("test-4.4.txt")
+    print("4 inst: {}, {}, {}, {}".format((avg1+avg2+avg3+avg4)/4, min(minv1,minv2,minv3,minv4), max(maxv1,maxv2,maxv3,maxv4), (pmiss1+pmiss2+pmiss3+pmiss4)/4))
